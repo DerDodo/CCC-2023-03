ea101e9864942fd1c5208c8cb0ece70202ba770b
Franz B <franz.buchsbaum@kununu.com>
2023-03-31 15:18:59 +0200

    Implement level1

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from file_management import read_input_lines, print_result_lines
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press Strg+F8 to toggle the breakpoint.
+def level1(file_id: int | str):
+    lines = read_input_lines(1, file_id)
+    results = []
+    for i in range(int(lines[0]) - 1):
+        match "".join(sorted(lines[i + 1].strip())):
+            # PRS
+            case "PP":
+                results += "P"
+            case "PR":
+                results += "P"
+            case "PS":
+                results += "S"
+            case "RR":
+                results += "R"
+            case "RS":
+                results += "R"
+            case "SS":
+                results += "S"
+            case _:
+                raise ValueError
+
+    print_result_lines(1, file_id, results)
+    return results
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    print_hi("PyCharm")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    level1()
